6b751a33f17defe1a34c0792506b236b7a8bebce
IgnasVanagas <ignas.judo@gmail.com>
2025-04-28 16:20:41 +0300

    swagger

--- fridgeshare-api/FridgeShare/Program.cs
+++ fridgeshare-api/FridgeShare/Program.cs
@@ -1,22 +1,33 @@
 using FridgeShare.Services.Products;
 using FridgeShare.Services.Storages;
 using FridgeShare.Services.Communities;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 
 
 var builder = WebApplication.CreateBuilder(args);
 
-{
-    builder.Services.AddControllers();
-    builder.Services.AddScoped<IProductService, productService>();
-    builder.Services.AddScoped<IStorageService, storageService>();
-    builder.Services.AddScoped<ICommunityService, CommunityService>();
-}
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();  
+builder.Services.AddSwaggerGen();            
+
+builder.Services.AddScoped<IProductService, productService>();
+builder.Services.AddScoped<IStorageService, storageService>();
+builder.Services.AddScoped<ICommunityService, CommunityService>();
 
 var app = builder.Build();
 
+
+if (app.Environment.IsDevelopment())  
 {
-    app.UseExceptionHandler("/error");
-    app.UseHttpsRedirection();
-    app.MapControllers();
-    app.Run();
+    app.UseSwagger();                 
+    app.UseSwaggerUI();                
 }
+
+app.UseExceptionHandler("/error");
+app.UseHttpsRedirection();
+app.MapControllers();
+app.Run();
