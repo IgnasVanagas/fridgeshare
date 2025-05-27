2f56229ca2f2bab43fd9f16dff0759a484abbdf8
Marius Varna <118673872+MariusVarna@users.noreply.github.com>
2025-05-27 18:01:36 +0300

    Update FridgeShareDbContextModelSnapshot.cs

--- fridgeshare-api/FridgeShare/Migrations/FridgeShareDbContextModelSnapshot.cs
+++ fridgeshare-api/FridgeShare/Migrations/FridgeShareDbContextModelSnapshot.cs
@@ -17,7 +17,7 @@ namespace FridgeShare.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "8.0.4")
+                .HasAnnotation("ProductVersion", "9.0.4")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
