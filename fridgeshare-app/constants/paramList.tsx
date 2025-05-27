a4f37afecd18b6c6622f0d7bcbab0f5e226b9c06
Marius Varna <118673872+MariusVarna@users.noreply.github.com>
2025-05-27 17:56:05 +0300

    Update paramList.tsx

--- fridgeshare-app/constants/paramList.tsx
+++ fridgeshare-app/constants/paramList.tsx
@@ -1,16 +1,22 @@
+import { Community } from './communityType';
 import { Storage } from './storage';
 import { Tag } from './tag';
 export type ParamList = {
 	CommunityView: { id: number };
+	EditCommunity: { id: number };
 	Drawer: undefined;
 	AuthIndex: undefined;
 	RequestList: undefined;
 	JoinCommunity: undefined;
 	CreateCommunity: undefined;
+	AddStorage: { communityId: number; storage?: Storage; adminAdd: boolean };
 	AddStorage: { communityId: number; storage?: Storage };
 	StorageList: { communityId: number; isAdmin: boolean };
 	AddTag: { communityId: number; tag?: Tag };
 	TagsList: { communityId: number; isAdmin: boolean };
+	CommunitySettings: { community: Community };
+	PendingRequests: { communityId: number };
+	AllUsersList: { communityId: number; isAdmin: boolean };
 	CommunityList: undefined;
 	AddProduct: { communityId?: number };
 	ChangeUsername: undefined;
